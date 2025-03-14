6c63da1a2de0f23eedd6f3b3b22c6e0dd837a39a
Laura Riascos <lauris.riascos@gmail.com>
2025-03-14 17:06:29 -0500

    se agregaron constantes y variables

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+
+//---------------1. Comentarios
+
+/*
+    Esto es un gran comentario!
+*/
+
+//------------ 2. Variables
+
+
+//Variable que puede cambiar su valor
+let variableLet = "Hola"
+
+console.log(variableLet)
+
+variableLet = "Mundo!"
+
+console.log(variableLet)
+
+variableLet = "Esto es una variable"
+
+console.log(variableLet)
+
+//Constante (sirve para datos que se repiten varias veces)
+
+const constanteConts = "Esto es una constante"
+
+console.log(constanteConts)
+
+//Var (sirve para asignar un valor)
+
+var Esmayor = true
+
